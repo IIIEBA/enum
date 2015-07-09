fcd21d26a07a5b2cdecb9536b69063bda03a2428
Andrew shevchuk <shevchuk@be-aware.com.ua>
2015-07-09 11:38:56 +0300

    Add basic Enum functionality

--- src/Enum/Lib/Enum.php
+++ src/Enum/Lib/Enum.php
@@ -2,7 +2,63 @@
 
 namespace Enum\Lib;
 
+use Enum\Lib\Exception\EnumException;
+
+/**
+ * Class Enum
+ * @package Enum\Lib
+ */
 abstract class Enum
 {
+    /**
+     * @var mixed
+     */
+    private $value;
 
+    /**
+     * @var string
+     */
+    private $key;
+
+    /**
+     * Create enum object
+     * @param $value
+     * @throws EnumException
+     */
+    public function __construct($value)
+    {
+        if(!is_scalar($value)) {
+            throw new \InvalidArgumentException('Only scalar types are allowed for value');
+        }
+
+        $reflection = new \ReflectionClass($this);
+        $constants  = $reflection->getConstants();
+
+        if (count($constants) === 0) {
+            throw new EnumException('You must set at least one constant to use Enum object');
+        }
+
+        if (!in_array($value, array_values($constants))) {
+            throw new \InvalidArgumentException('Not allowed enum value was given');
+        }
+
+        $this->value = $value;
+        $this->key = array_flip($constants)[$value];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
 }
---
+++ src/Enum/Lib/Exception/EnumException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Enum\Lib\Exception;
+
+/**
+ * Class EnumException
+ * @package Enum\Lib\Exception
+ */
+class EnumException extends \Exception
+{
+
+}
